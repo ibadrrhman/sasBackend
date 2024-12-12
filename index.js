50fe351da508a5954dc5da419e1be74d5856bcd2
ibadurrohman <=>
2024-12-12 07:39:32 +0700

    local to online

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import ProductRoute from './router/ProductRouter.js'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import ProductRoute from "./router/ProductRouter.js";
 
 const app = express();
 // mongoose.connect("mongodb://localhost:27017/fullstack_db", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // });
-mongoose.connect("mongodb://localhost:27017/restful_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db=mongoose.connection;
-db.on("error", (error)=>console.log(error))
-db.once("open", ()=> console.log("Success connected..."))
+mongoose.connect(
+  "mongodb+srv://ibadurrohmannn:f5pIHKkF4Bd7Bufc@cluster0.bvxrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Success connected..."));
 
-app.use(cors())
-app.use(express.json())
-app.use(ProductRoute)
+app.use(cors());
+app.use(express.json());
+app.use(ProductRoute);
 
-app.listen(6000,()=>console.log("Server running..."))
+app.listen(6001, () => console.log("Server running..."));
